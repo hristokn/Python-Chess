7c51e218502a54e458eaffdb465ced14d8973d28
hristokn <115088995+hristokn@users.noreply.github.com>
2024-08-14 18:26:07 +0300

    change tuple to TupleN in game_objects, add ChessBoardObjectCollider class

--- game_objects.py
+++ game_objects.py
@@ -1,4 +1,6 @@
-import pygame
+import pygame.surface
+import pygame.event
+from tuple_n import TupleN
 
 
 class GameObject:
@@ -11,30 +13,36 @@ class GameObject:
 
 
 class Drawable:
-    def __init__(self, pos: tuple[int, int], image: pygame.surface = None, **kwargs):
+    def __init__(
+        self, pos: TupleN[int, int], image: pygame.surface.Surface = None, **kwargs
+    ):
         super().__init__(**kwargs)
         self.image = image
         self.move_drawable(pos)
 
-    def draw(self, surface: pygame.surface):
+    def draw(self, surface: pygame.Surface):
         if self.image != None:
             surface.blit(self.image, self.pos)
 
-    def move_drawable(self, pos: tuple[int, int]):
+    def move_drawable(self, pos: TupleN[int, int]):
         self.pos = pos
 
 
 class Collider:
 
-    def __init__(self, coords: tuple[int, int], dimensions: tuple[int, int], **kwargs):
+    def __init__(
+        self, coords: TupleN[int, int], dimensions: TupleN[int, int], **kwargs
+    ):
         super().__init__(**kwargs)
         self.move_collider(coords, dimensions)
 
-    def move_collider(self, coords: tuple[int, int], dimensions: tuple[int, int]):
+    def move_collider(
+        self, coords: TupleN[int, int], dimensions: TupleN[int, int] = None
+    ):
         self.coords = coords
-        self.dimensions = dimensions
+        self.dimensions = dimensions if dimensions != None else self.dimensions
 
-    def clicked(self, coords: tuple[int, int]):
+    def clicked(self, coords: TupleN[int, int]):
         x = coords[0]
         y = coords[1]
         self_x = self.coords[0]
@@ -50,7 +58,7 @@ class Collider:
             return False
         return True
 
-    def handle_click(self, click: pygame.event):
+    def handle_click(self, click: pygame.event.Event):
         if click.type == pygame.MOUSEBUTTONDOWN and click.button == 1:
             self.handle_left_button_down()
         if click.type == pygame.MOUSEBUTTONUP and click.button == 1:
@@ -59,6 +67,7 @@ class Collider:
             self.handle_right_button_up()
         if click.type == pygame.MOUSEBUTTONDOWN and click.button == 2:
             self.handle_right_button_down()
+        raise ValueError
 
     def handle_left_button_down(self):
         pass
@@ -73,11 +82,37 @@ class Collider:
         pass
 
 
-class ChessBoardObject(Collider, GameObject, Drawable):
-    def __init__(self, board, **kwargs):
+class ChessBoardObjectCollider(Collider):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.board = board
+        self.clicks = []
+
+    def handle_click(self, click: pygame.event.Event):
+        self.clicks.append(click)
+
+    def peek_click(self) -> None | pygame.event.Event:
+        try:
+            return self.clicks[0]
+        except IndexError:
+            return None
+
+    def pop_click(self) -> None | pygame.event.Event:
+        try:
+            return self.clicks.pop(0)
+        except IndexError:
+            return None
+
+
+class ChessBoardObject(ChessBoardObjectCollider, GameObject, Drawable):
+    def __init__(self, square: TupleN[int, int], **kwargs):
+        super().__init__(**kwargs)
         self.selected = False
+        self.square: TupleN[int, int] = square
 
     def is_empty(self):
         return True
+
+    def move(self, square: TupleN[int, int], screen_coords: TupleN[int, int]):
+        self.square = square
+        self.move_collider(screen_coords)
+        self.move_drawable(screen_coords)
