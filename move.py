a73d251ab8dc30c57f329a22a24b4cf06874078f
hristokn <115088995+hristokn@users.noreply.github.com>
2024-08-14 18:22:33 +0300

    fixed bug and added eq and hash functions to Move class

--- move.py
+++ move.py
@@ -4,4 +4,10 @@ from tuple_n import TupleN
 class Move:
     def __init__(self, start: TupleN[int, int], end: TupleN[int, int]):
         self.start = start
-        self.end = start
+        self.end = end
+
+    def __eq__(self, other):
+        return self.start == other.start and self.end == other.end
+
+    def __hash__(self):
+        return hash((self.start, self.end))
