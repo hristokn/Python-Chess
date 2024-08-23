40cacd8317a082d0531b2457aa45178353fb6417
hristokn <115088995+hristokn@users.noreply.github.com>
2024-08-23 23:25:17 +0300

    many changes in chess board and pieces

--- chess_board.py
+++ chess_board.py
@@ -22,14 +22,69 @@ class ChessBoard(Collider, GameObject, Drawable):
         )
         self.game_images = game_images
         self.game_custom_events = game_custom_events
-        self.chess_board = []
+        self.chess_board: list[list[ChessBoardObject]] = []
         self.objects = []
-        self._fill_board()
-        self.selected = None
+        self.selected_piece = None
         self.past_moves = []
         self.possible_moves = []
         self.color_to_move = Color.WHITE
         self.taken_pieces = []
+
+        self._fill_board()
+        self.calculate_potential_moves()
+
+    def update(self):
+        super().update()
+        for row in self.chess_board:
+            for obj in row:
+                click = obj.pop_click()
+                if click == None:
+                    continue
+                elif click.button == 1:
+                    if obj.is_empty():
+                        self.square_clicked(obj, click)
+                    else:
+                        self.piece_clicked(obj, click)
+        for obj in self.objects:
+            click = obj.pop_click()
+            if click == None:
+                continue
+            elif click.type == pygame.MOUSEBUTTONUP and click.button == 1:
+                self.obj_clicked(obj, click)
+            elif click.type == pygame.MOUSEBUTTONDOWN:
+                pass
+            else:
+                raise ValueError
+
+    def obj_clicked(self, obj, click):
+        # left click on piece, up or down
+        move_start = self.selected_piece.square
+        move_end = obj.square
+        move = Move(move_start, move_end)
+        self.selected_piece = None
+        self.play_move(move)
+
+    def square_clicked(self, obj, click):
+        downclick = click.type == pygame.MOUSEBUTTONDOWN
+        if downclick:
+            if self.selected_piece == None:
+                pass
+            else:
+                self.deselect_piece()
+
+    def piece_clicked(self, piece, click):
+        # left click on piece, up or down
+        downclick = click.type == pygame.MOUSEBUTTONDOWN
+        if downclick:
+            if self.selected_piece == piece:
+                pass
+            else:
+                self.select_piece(piece)
+        else:
+            if self.selected_piece == piece:
+                pass
+            else:
+                pass
 
     def _fill_board(self):
 
@@ -37,61 +92,65 @@ class ChessBoard(Collider, GameObject, Drawable):
             self.chess_board.append([])
             for column in range(8):
                 self.chess_board[row].append(None)
-                self.add_empty_square((row, column))
+                self.add_empty_square(TupleN((row, column)))
 
-        rook = Rook(self, False, self.game_images["black_rook"])
-        self._fill_square([0, 0], rook)
-        rook = Rook(self, False, self.game_images["black_rook"])
-        self._fill_square([0, 7], rook)
+        rook = Rook(Color.BLACK, self.game_images["black_rook"], TupleN((0, 0)))
+        self._fill_square(rook)
+        rook = Rook(Color.BLACK, self.game_images["black_rook"], TupleN((0, 7)))
+        self._fill_square(rook)
 
-        bishop = Bishop(self, False, self.game_images["black_bishop"])
-        self._fill_square([0, 1], bishop)
-        bishop = Bishop(self, False, self.game_images["black_bishop"])
-        self._fill_square([0, 6], bishop)
+        bishop = Bishop(Color.BLACK, self.game_images["black_bishop"], TupleN((0, 1)))
+        self._fill_square(bishop)
+        bishop = Bishop(Color.BLACK, self.game_images["black_bishop"], TupleN((0, 6)))
+        self._fill_square(bishop)
 
-        knight = Knight(self, False, self.game_images["black_knight"])
-        self._fill_square([0, 2], knight)
-        knight = Knight(self, False, self.game_images["black_knight"])
-        self._fill_square([0, 5], knight)
+        knight = Knight(Color.BLACK, self.game_images["black_knight"], TupleN((0, 2)))
+        self._fill_square(knight)
+        knight = Knight(Color.BLACK, self.game_images["black_knight"], TupleN((0, 5)))
+        self._fill_square(knight)
 
-        queen = Queen(self, False, self.game_images["black_queen"])
-        self._fill_square([0, 3], queen)
-        king = King(self, False, self.game_images["black_king"])
-        self._fill_square([0, 4], king)
-
-        for column in range(8):
-            pawn = Pawn(self, False, self.game_images["black_pawn"])
-            self._fill_square([1, column], pawn)
-
-        rook = Rook(self, True, self.game_images["white_rook"])
-        self._fill_square([7, 0], rook)
-        rook = Rook(self, True, self.game_images["white_rook"])
-        self._fill_square([7, 7], rook)
-
-        bishop = Bishop(self, True, self.game_images["white_bishop"])
-        self._fill_square([7, 1], bishop)
-        bishop = Bishop(self, True, self.game_images["white_bishop"])
-        self._fill_square([7, 6], bishop)
-
-        knight = Knight(self, True, self.game_images["white_knight"])
-        self._fill_square([7, 2], knight)
-        knight = Knight(self, True, self.game_images["white_knight"])
-        self._fill_square([7, 5], knight)
-
-        queen = Queen(self, True, self.game_images["white_queen"])
-        self._fill_square([7, 3], queen)
-        king = King(self, True, self.game_images["white_king"])
-        self._fill_square([7, 4], king)
+        queen = Queen(Color.BLACK, self.game_images["black_queen"], TupleN((0, 3)))
+        self._fill_square(queen)
+        king = King(Color.BLACK, self.game_images["black_king"], TupleN((0, 4)))
+        self._fill_square(king)
 
         for column in range(8):
-            pawn = Pawn(self, True, self.game_images["white_pawn"])
-            self._fill_square([6, column], pawn)
+            pawn = Pawn(
+                Color.BLACK, self.game_images["black_pawn"], TupleN((1, column))
+            )
+            self._fill_square(pawn)
+
+        rook = Rook(Color.WHITE, self.game_images["white_rook"], TupleN((7, 0)))
+        self._fill_square(rook)
+        rook = Rook(Color.WHITE, self.game_images["white_rook"], TupleN((7, 7)))
+        self._fill_square(rook)
+
+        bishop = Bishop(Color.WHITE, self.game_images["white_bishop"], TupleN((7, 1)))
+        self._fill_square(bishop)
+        bishop = Bishop(Color.WHITE, self.game_images["white_bishop"], TupleN((7, 6)))
+        self._fill_square(bishop)
+
+        knight = Knight(Color.WHITE, self.game_images["white_knight"], TupleN((7, 2)))
+        self._fill_square(knight)
+        knight = Knight(Color.WHITE, self.game_images["white_knight"], TupleN((7, 5)))
+        self._fill_square(knight)
+
+        queen = Queen(Color.WHITE, self.game_images["white_queen"], TupleN((7, 3)))
+        self._fill_square(queen)
+        king = King(Color.WHITE, self.game_images["white_king"], TupleN((7, 4)))
+        self._fill_square(king)
+
+        for column in range(8):
+            pawn = Pawn(
+                Color.WHITE, self.game_images["white_pawn"], TupleN((6, column))
+            )
+            self._fill_square(pawn)
 
         for row in self.chess_board:
             for square in row:
                 self._add_object(square)
 
-    def _convert_square_to_screen_position(self, square: tuple[int, int]):
+    def _convert_square_to_screen_position(self, square: TupleN[int, int]):
         dimensions = (self.dimensions[0] / 8, self.dimensions[1] / 8)
         coords = (
             self.coords[0] + square[1] * dimensions[0],
@@ -100,16 +159,19 @@ class ChessBoard(Collider, GameObject, Drawable):
 
         return (coords, dimensions)
 
-    def _fill_square(self, square: tuple[int, int], object: ChessBoardObject):
+    def _fill_square(
+        self, object: ChessBoardObject, square: TupleN[int, int] | None = None
+    ):
+        square = square if square != None else object.square
         empty_square = self.chess_board[square[0]][square[1]]
         object.coords = empty_square.coords
         object.dimensions = empty_square.dimensions
         object.pos = empty_square.pos
         self.chess_board[square[0]][square[1]] = object
 
-    def add_empty_square(self, square):
+    def add_empty_square(self, square: TupleN[int, int]):
         screen_pos = self._convert_square_to_screen_position(square)
-        object = EmptySquare(self, screen_pos[0], screen_pos[1])
+        object = EmptySquare(screen_pos[0], screen_pos[1], square)
         self.chess_board[square[0]][square[1]] = object
 
     def _remove_object(self, object: GameObject):
@@ -140,39 +202,39 @@ class ChessBoard(Collider, GameObject, Drawable):
         return self.chess_board[square[0]][square[1]]
 
     def get_selected_piece(self):
-        return self.selected
+        return self.selected_piece
 
     def select_piece(self, piece: ChessBoardObject):
         self.deselect_piece()
         if piece.is_empty():
             return
 
-        self.selected = piece
+        self.selected_piece = piece
         self.highlight_potential_moves()
 
     def highlight_potential_moves(self, piece: ChessBoardObject | None = None):
         if piece == None:
-            piece = self.selected
+            piece = self.selected_piece
         if piece.is_empty():
             raise Exception("Empty square passed as argument")
 
-        square = self.get_square(piece)
-        valid_moves = filter(lambda move: move.start == square, self.possible_moves)
+        square = piece.square
+        valid_moves = [move for move in self.possible_moves if move.start == square]
         for move in valid_moves:
             piece = self.get_piece(move.end)
             if piece.is_empty():
-                self.add_valid_move_object(move)
+                self.add_valid_move_object(move.end)
             else:
-                self.add_valid_take_object(move)
+                self.add_valid_take_object(move.end)
 
     def add_valid_move_object(self, square: tuple[int, int]):
         screen_position = self._convert_square_to_screen_position(square)
         move = ValidMove(
-            self,
             screen_position[0],
             screen_position[1],
-            self.game_images["valid_move_symbol"],
+            self.game_images["valid_move"],
             screen_position[0],
+            square,
         )
         self.objects.append(move)
         self._add_object(move)
@@ -183,14 +245,17 @@ class ChessBoard(Collider, GameObject, Drawable):
             self,
             screen_position[0],
             screen_position[1],
-            self.game_images["take_symbol"],
+            self.game_images["valid_take"],
             screen_position[0],
         )
         self.objects.append(take)
         self._add_object(take)
 
     def deselect_piece(self):
-        self.selected = None
+        self.selected_piece = None
+        self.unhighlight_potential_moves()
+
+    def unhighlight_potential_moves(self):
         for object in self.objects:
             self._remove_object(object)
         self.objects = []
@@ -208,19 +273,17 @@ class ChessBoard(Collider, GameObject, Drawable):
         )
 
     def calculate_potential_moves(self, color: Color | None = None):
-        self.moves = []
+        self.possible_moves = []
         if color == None:
-            color = self.color
+            color = self.color_to_move
 
-        for row in ChessBoard:
+        for row in self.chess_board:
             for piece in row:
                 if not piece.is_empty() and piece.color == color:
-                    self.possible_moves += piece.get_moves(
-                        self.get_square(piece), self.chess_board
-                    )
+                    self.possible_moves.extend(piece.get_moves(self.chess_board))
 
     def play_move(self, move: Move):
-        if not move in self.possible_moves:
+        if move not in self.possible_moves:
             raise Exception("Tried to play invalid move")
 
         if self.get_piece(move.end).is_empty():
@@ -235,7 +298,9 @@ class ChessBoard(Collider, GameObject, Drawable):
     def move(self, move: Move):
         start_piece = self.get_piece(move.start)
         end_square = self.get_piece(move.end)
+        start_piece.move(move.start, end_square.pos)
         self.chess_board[move.end[0]][move.end[1]] = start_piece
+        end_square.move(move.start, start_piece.pos)
         self.chess_board[move.start[0]][move.start[1]] = end_square
 
     def take(self, move: Move):
@@ -252,16 +317,16 @@ class ChessBoard(Collider, GameObject, Drawable):
 class EmptySquare(ChessBoardObject):
     def __init__(
         self,
-        board: ChessBoard,
         coords: tuple[int, int],
         dimensions: tuple[int, int],
+        square: TupleN[int, int],
         depth: int = -9,
     ):
         super().__init__(
-            board=board,
             coords=coords,
             dimensions=dimensions,
             depth=depth,
+            square=square,
             pos=coords,
         )
 
@@ -272,15 +337,15 @@ class EmptySquare(ChessBoardObject):
 class ValidMove(ChessBoardObject):
     def __init__(
         self,
-        board: ChessBoard,
         coords: tuple[int, int],
         dimensions: tuple[int, int],
         image: pygame.surface,
         pos: tuple[int, int],
+        square: TupleN[int, int],
         depth: int = 1,
     ):
         super().__init__(
-            board=board,
+            square=square,
             coords=coords,
             dimensions=dimensions,
             depth=depth,
@@ -298,20 +363,20 @@ class ValidMove(ChessBoardObject):
 class ValidTake(ChessBoardObject):
     def __init__(
         self,
-        board: ChessBoard,
         coords: tuple[int, int],
         dimensions: tuple[int, int],
         image: pygame.surface,
         pos: tuple[int, int],
+        square: TupleN[int, int],
         depth: int = 1,
     ):
         super().__init__(
-            board=board,
             coords=coords,
             dimensions=dimensions,
             depth=depth,
             pos=pos,
             image=image,
+            square=square,
         )
 
     def handle_left_button_down(self):
--- chess_pieces.py
+++ chess_pieces.py
@@ -5,14 +5,11 @@ from tuple_n import TupleN
 from my_enums import Color
 
 
-class ChessBoard:
-    pass
-
-
 class Piece(ChessBoardObject):
     def __init__(self, color: Color, **kwargs):
         super().__init__(**kwargs)
         self.color = color
+        self.has_moved = False
 
     def is_empty(self):
         return False
@@ -20,194 +17,181 @@ class Piece(ChessBoardObject):
     def handle_left_button_down(self):
         self.board.select_piece(self)
 
-    def get_moves(self):
+    def get_moves(self, chess_board: list[list[ChessBoardObject]]) -> list[Move]:
         return []
+
+    def move(self, square: TupleN[int, int], screen_coords: TupleN[int, int]):
+        super().move(square, screen_coords)
+        self.has_moved = True
 
 
 class Queen(Piece):
     def __init__(
         self,
-        board: ChessBoard,
-        is_white: bool,
+        color: Color,
         image: pygame.Surface,
+        square: TupleN[int, int],
         coords: tuple[int, int] = 0,
         dimensions: tuple[int, int] = 0,
         pos: tuple[int, int] = 0,
         depth: int = 0,
     ):
         super().__init__(
-            board=board,
-            is_white=is_white,
+            square=square,
+            color=color,
             coords=coords,
             dimensions=dimensions,
             depth=depth,
             pos=pos,
             image=image,
         )
-
-    def move_piece(self):
-        pass
 
 
 class King(Piece):
     def __init__(
         self,
-        board: ChessBoard,
-        is_white: bool,
+        color: Color,
         image: pygame.Surface,
+        square: TupleN[int, int],
         coords: tuple[int, int] = 0,
         dimensions: tuple[int, int] = 0,
         pos: tuple[int, int] = 0,
         depth: int = 0,
     ):
         super().__init__(
-            board=board,
-            is_white=is_white,
+            square=square,
+            color=color,
             coords=coords,
             dimensions=dimensions,
             depth=depth,
             pos=pos,
             image=image,
         )
-
-    def move_piece(self):
-        pass
 
 
 class Bishop(Piece):
     def __init__(
         self,
-        board: ChessBoard,
-        is_white: bool,
+        color: Color,
         image: pygame.Surface,
+        square: TupleN[int, int],
         coords: tuple[int, int] = 0,
         dimensions: tuple[int, int] = 0,
         pos: tuple[int, int] = 0,
         depth: int = 0,
     ):
         super().__init__(
-            board=board,
-            is_white=is_white,
+            square=square,
+            color=color,
             coords=coords,
             dimensions=dimensions,
             depth=depth,
             pos=pos,
             image=image,
         )
-
-    def move_piece(self):
-        pass
 
 
 class Knight(Piece):
     def __init__(
         self,
-        board: ChessBoard,
-        is_white: bool,
+        color: Color,
         image: pygame.Surface,
+        square: TupleN[int, int],
         coords: tuple[int, int] = 0,
         dimensions: tuple[int, int] = 0,
         pos: tuple[int, int] = 0,
         depth: int = 0,
     ):
         super().__init__(
-            board=board,
-            is_white=is_white,
+            square=square,
+            color=color,
             coords=coords,
             dimensions=dimensions,
             depth=depth,
             pos=pos,
             image=image,
         )
-
-    def move_piece(self):
-        pass
 
 
 class Rook(Piece):
     def __init__(
         self,
-        board: ChessBoard,
-        is_white: bool,
+        color: Color,
         image: pygame.Surface,
+        square: TupleN[int, int],
         coords: tuple[int, int] = 0,
         dimensions: tuple[int, int] = 0,
         pos: tuple[int, int] = 0,
         depth: int = 0,
     ):
         super().__init__(
-            board=board,
-            is_white=is_white,
+            color=color,
             coords=coords,
             dimensions=dimensions,
             depth=depth,
             pos=pos,
             image=image,
+            square=square,
         )
-
-    def move_piece(self):
-        pass
 
 
 class Pawn(Piece):
     def __init__(
         self,
-        board: ChessBoard,
-        is_white: bool,
+        color: Color,
         image: pygame.Surface,
+        square: TupleN[int, int],
         coords: tuple[int, int] = 0,
         dimensions: tuple[int, int] = 0,
         pos: tuple[int, int] = 0,
         depth: int = 0,
     ):
         super().__init__(
-            board=board,
-            is_white=is_white,
+            color=color,
             coords=coords,
             dimensions=dimensions,
             depth=depth,
             pos=pos,
             image=image,
+            square=square,
         )
 
-    def get_moves(
-        self, square: TupleN[int, int], chess_board: list[ChessBoardObject]
-    ) -> list[Move]:
+    def get_moves(self, chess_board: list[list[ChessBoardObject]]) -> list[Move]:
         valid_squares = []
 
         try:
             forward_square = (
-                square + TupleN((-1, 0))
+                self.square + TupleN((-1, 0))
                 if self.color == Color.WHITE
-                else square + TupleN((1, 0))
+                else self.square + TupleN((1, 0))
             )
             piece = chess_board[forward_square[0]][forward_square[1]]
             if piece.is_empty():
-                valid_squares.append(Move(square, forward_square))
+                valid_squares.append(Move(self.square, forward_square))
         except IndexError:
             pass
 
         try:
             left_diagonal_square = (
-                square + TupleN((-1, -1))
+                self.square + TupleN((-1, -1))
                 if self.color == Color.WHITE
-                else square + TupleN((1, -1))
+                else self.square + TupleN((1, -1))
             )
             piece = chess_board[left_diagonal_square[0]][left_diagonal_square[1]]
             if not piece.is_empty() and self.color != piece.color:
-                valid_squares.append(Move(square, left_diagonal_square))
+                valid_squares.append(Move(self.square, left_diagonal_square))
         except IndexError:
             pass
 
         try:
             right_diagonal_square = (
-                square + TupleN((-1, 1))
+                self.square + TupleN((-1, 1))
                 if self.color == Color.WHITE
-                else square + TupleN((1, -1))
+                else self.square + TupleN((1, -1))
             )
             piece = chess_board[right_diagonal_square[0]][right_diagonal_square[1]]
             if not piece.is_empty() and self.color != piece.color:
-                valid_squares.append(Move(square, right_diagonal_square))
+                valid_squares.append(Move(self.square, right_diagonal_square))
         except IndexError:
             pass
 
