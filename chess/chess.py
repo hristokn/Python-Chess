728a7931415ddd7217d3a582fb458ddfaeeb9e34
hristokn <115088995+hristokn@users.noreply.github.com>
2024-09-21 22:17:55 +0300

    removed print method, play_move now takes piece, square and returns bool if move was played, added method to find a square from a piece

--- chess/chess.py
+++ chess/chess.py
@@ -4,56 +4,70 @@ from chess.squares import Square, WhiteOrientation
 from chess.moves import Move
 
 class ChessBoard:
-    def __init__(self):
+    def __init__(self, color: Color):
         self.board = {}
         self.standard_board(self.board)
         self.past_moves = []
         self.possible_moves = []
         self.taken_pieces = []
-        self.color_to_play = Color.WHITE
+        self.color_to_play = color
+
 
     def calc_possible_moves(self, color: Color):
         self.possible_moves = []
         for sq, piece in self.board.items():
             if piece != None and piece.color == color:
                 self.possible_moves.extend(piece.get_moves(sq, self.past_moves, self.board))
-            
+
+
     def start(self):
-        self.prepare_turn()
+        self.calc_possible_moves(self.color_to_play)
 
-    def play_move(self, move: Move):
-        if not move in self.possible_moves:
-            return False
-        
-        self.taken_pieces.extend(move.taken)
-        for sq, piece in move.changes.items():
-            self.board[sq] = piece
-        return True
 
-    def print_possible_moves(self):
-        i = 1
-        for move in self.possible_moves:
-            print(str(i) + ' ' + str(move))
-            i = i + 1
+    def find_square(self, piece: Piece) -> Square:
+        for sq, p in self.board.items():
+            if piece == p:
+                return sq
+        return Square.UNKNOWN
+
+
+    def play_move(self, start: Piece, end: Square):
+        move = None
+        for m in self.possible_moves:
+            if m.described_by(start, end):
+                move = m
+                break
+
+        if move != None:
+            self.taken_pieces.extend(move.taken)
+            for sq, piece in move.changes.items():
+                self.board[sq] = piece
+            self.prepare_turn()
+            return True
+        return False
+
 
     def prepare_turn(self):
+        self.next_color()
         self.calc_possible_moves(self.color_to_play)
+
 
     def turn(self, move: Move):
         self.play_move(move)
-        self.next_color()
         self.prepare_turn()
-        
+
+
     def next_color(self):
         if self.color_to_play == Color.WHITE:
             self.color_to_play = Color.BLACK
         elif self.color_to_play == Color.BLACK:
             self.color_to_play = Color.WHITE
 
+
     def standard_board(self, board):
         for square in range(64):
             board[Square(square)] = None
-        
+
         sq = Square.A2
         for _ in range(8):
             board[sq] = Piece(Color.WHITE, PieceType.PAWN)
